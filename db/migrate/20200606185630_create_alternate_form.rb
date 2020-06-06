50f75dd6a8dd1352464c9a4b010f9c1f48b25c37
Deonte Cooper <deonte.cooper12@gmail.com>
2020-06-06 12:59:36 -0600

    Add Migration for Alternate Forms

---
+++ db/migrate/20200606185630_create_alternate_form.rb
@@ -0,0 +1,23 @@
+class CreateAlternateForm < ActiveRecord::Migration[6.0]
+  def change
+    create_table :alternate_forms do |t|
+      t.string :pokedex_number
+      t.string :name
+      t.string :type_1
+      t.string :type_2
+      t.string :ability_1
+      t.string :ability_2
+      t.string :ability_3
+      t.string :ability_4
+      t.integer :hp
+      t.integer :attack
+      t.integer :defense
+      t.integer :special_attack
+      t.integer :special_defense
+      t.integer :speed
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
