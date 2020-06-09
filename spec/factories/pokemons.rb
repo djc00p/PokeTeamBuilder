495b75e5cbe3c5e065f9e91bfcb3602cb79ce624
Deonte Cooper <deonte.cooper12@gmail.com>
2020-06-08 21:02:44 -0600

    Add Pokemon Factories

---
+++ spec/factories/pokemons.rb
@@ -0,0 +1,19 @@
+FactoryBot.define do
+  factory :pokemon do
+    dex_number { rand(1..400) }
+    name { Faker::Games::Pokemon.name }
+    type_1 { ["Normal","Fire","Water","Grass","Electric","Ice","Fighting","Poison","Ground","Flying","Psychic","Bug","Rock","Ghost","Dark","Dragon","Steel","Fairy"] }
+    type_2 { ["Normal","Fire","Water","Grass","Electric","Ice","Fighting","Poison","Ground","Flying","Psychic","Bug","Rock","Ghost","Dark","Dragon","Steel","Fairy","none"] }
+    ability_1 { ["Blaze","Torrent","Overgrow","Swarm","Cheek Pouch"] }
+    ability_2 { ["Ball Fetch","Chlorophyll","Fluffy","none","none"] }
+    ability_3 { ["Cute Charm","Oblivious","none","none","none"] }
+    ability_4 { ["Grassy Surge","none","none","none","none"] }
+    hp { rand(50..140) }
+    attack { rand(50..140) }
+    defense { rand(50..140) }
+    special_attack { rand(50..140) }
+    special_defense { rand(50..140) }
+    speed { rand(50..140) }
+    image_url { "url.picture" }
+  end
+end
