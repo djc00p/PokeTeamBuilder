9052845a72497fbccd0b969682ee4069bcf66708
Deonte Cooper <deonte.cooper12@gmail.com>
2020-06-09 14:39:37 -0600

    Add Validations for Nature Model

---
+++ spec/models/nature_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Ability, type: :model do
+  describe "Validations" do
+    it { should validate_presence_of :nature }
+    it { should validate_presence_of :increases }
+    it { should validate_presence_of :decreases }
+  end
+end
