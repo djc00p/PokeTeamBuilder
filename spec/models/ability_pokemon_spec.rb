7a2975e94b5274055c70e1392d296e3eb317e652
Deonte Cooper <deonte.cooper12@gmail.com>
2020-06-10 17:46:43 -0600

    Update AbilitiesPokemon test

--- spec/models/ability_pokemon_spec.rb
+++ spec/models/ability_pokemon_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe AbilityPokemon, type: :model do
+RSpec.describe AbilitiesPokemon, type: :model do
   describe "Relationships" do
     it { should belong_to :ability}
     it { should belong_to :pokemon}
