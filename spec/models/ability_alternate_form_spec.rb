a2fb122f3b351f3c6b6e36758f9d993e4b52e198
Deonte Cooper <deonte.cooper12@gmail.com>
2020-06-10 17:39:48 -0600

    Update test file for AbilitiesAlternateForm

--- spec/models/ability_alternate_form_spec.rb
+++ spec/models/ability_alternate_form_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe AbilityAlternateForm, type: :model do
+RSpec.describe AbilitiesAlternateForm, type: :model do
   describe "Relationships" do
     it { should belong_to :ability}
     it { should belong_to :alternate_form}
