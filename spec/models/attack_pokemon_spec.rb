b966c6e26f0da44d451f04d42cc7f88305fa015a
Deonte Cooper <deonte.cooper12@gmail.com>
2020-06-10 17:29:11 -0600

    Add AttackPokemon relationship test

---
+++ spec/models/attack_pokemon_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe AttackPokemon, type: :model do
+  describe "Relationships" do
+    it { should belong_to :attack}
+    it { should belong_to :pokemon}
+  end
+end
