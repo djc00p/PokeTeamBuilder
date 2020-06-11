0a1a496ff0e4a0c0367137bcc904a292f9568215
Deonte Cooper <deonte.cooper12@gmail.com>
2020-06-11 12:32:01 -0600

    Update alt form spec to belong to pokemon

--- spec/models/alternate_form_spec.rb
+++ spec/models/alternate_form_spec.rb
@@ -20,6 +20,7 @@ RSpec.describe AlternateForm, type: :model do
   end
 
   describe "Relationships" do
+    it {should belong_to :pokemons}
     it {should have_many :abilities_alternate_forms}
     it {should have_many(:abilities).through :abilities_alternate_forms}
     it {should have_many :alternate_forms_attacks}
