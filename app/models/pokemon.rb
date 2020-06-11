6eb5297031b7f78c65caae130497c779fd7e2c18
Deonte Cooper <deonte.cooper12@gmail.com>
2020-06-10 18:57:10 -0600

    Add Pokemon class Validations

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,3 +1,21 @@
 class Pokemon < ApplicationRecord
-
+  validates_presence_of :pokedex_number
+  validates_presence_of :name
+  validates_presence_of :type_1
+  validates_presence_of :type_2
+  validates_presence_of :ability_1
+  validates_presence_of :ability_2
+  validates_presence_of :ability_3
+  validates_presence_of :ability_4
+  validates_numericality_of :hp
+  validates_numericality_of :defense
+  validates_numericality_of :attack
+  validates_numericality_of :special_attack
+  validates_numericality_of :special_defense
+  validates_numericality_of :speed
+  validates_presence_of :image_url
+  has_many :abilities_pokemons
+  has_many :abilities, through: :abilities_pokemons
+  has_many :attacks_pokemons
+  has_many :attacks, through: :attacks_pokemons
 end
