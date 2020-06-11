5ced980ae72e1b63634eef4190da09fd6f9a86e6
Deonte Cooper <deonte.cooper12@gmail.com>
2020-06-10 18:45:02 -0600

    Add validations for Attack Class

--- app/models/attack.rb
+++ app/models/attack.rb
@@ -1,3 +1,12 @@
 class Attack < ApplicationRecord
-
+  validates_presence_of :name
+  validates_presence_of :type
+  validates_presence_of :pp
+  validates_presence_of :attack
+  validates_presence_of :accuracy
+  validates_presence_of :effect
+  validates_presence_of :attack_type
+  validates_presence_of :type_img
+  has_many :alternate_forms_attacks
+  has_many :alternate_forms, through: :alternate_forms_attacks
 end
