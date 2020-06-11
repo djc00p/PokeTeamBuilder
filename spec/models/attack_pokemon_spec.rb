2da0efcded071b1137741af652a4fdb51fbfe4d9
Deonte Cooper <deonte.cooper12@gmail.com>
2020-06-10 18:37:11 -0600

    Update spec for AttacksPokemon

--- spec/models/attack_pokemon_spec.rb
+++ spec/models/attack_pokemon_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe AttackPokemon, type: :model do
+RSpec.describe AttacksPokemon, type: :model do
   describe "Relationships" do
     it { should belong_to :attack}
     it { should belong_to :pokemon}
