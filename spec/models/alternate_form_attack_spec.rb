92802cd716ae2087eb81b4d110b49f09d8320340
Deonte Cooper <deonte.cooper12@gmail.com>
2020-06-10 18:34:50 -0600

    Updated AlternateFormAttack Spec

--- spec/models/alternate_form_attack_spec.rb
+++ spec/models/alternate_form_attack_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe AttackAlternateForm, type: :model do
+RSpec.describe AlternateFormsAttack, type: :model do
   describe "Relationships" do
     it { should belong_to :attack}
     it { should belong_to :alternate_form}
